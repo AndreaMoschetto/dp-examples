9af8caa12ca385ce69e751cf2c52d77e7dcaf131
Niria <andreamoschetto99@gmail.com>
2020-03-28 16:03:56 +0100

    add something

---
+++ ProvaPC/TestSingleton.java
@@ -0,0 +1,18 @@
+public class TestSingleton{
+    private Singleton single;
+    public void init_state(){
+        single = Singleton.getInstance();
+    }
+    public void testPrintSomething(){
+        init_state();
+        if(single.printSomething().equals("Something...XD"))
+            System.out.println("test Print something: SUCCESS");
+        else 
+        System.out.println("test Print something: FAILED");
+    }
+
+    public static void main(final String[] args) {
+        final TestSingleton test = new TestSingleton();
+        test.testPrintSomething();
+    }
+}
